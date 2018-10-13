2c840f940d203c2b0e0de814e5ed44f9083e74a3
prazd <alexokda@list.ru>
2018-10-13 22:03:06 +0300

    run

--- qt/main.py
+++ qt/main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 import ui
 
 class ExampleApp(QtWidgets.QMainWindow, ui.Ui_MainWindow):
@@ -8,8 +9,8 @@ class ExampleApp(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-        self.pushButton.clicked.connect(self.Clear)
-        self.pushButton_2.clicked.connect(self.Check)
+        self.clear.clicked.connect(self.Clear)
+        self.run.clicked.connect(self.Run)
 
     def Clear(self):
         # self.inpMonthIncome.setText("")
@@ -20,12 +21,15 @@ class ExampleApp(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         self.inpCashback.setText("")
         self.inpSaving.setText("")
         self.anotherIncome.setText("")
+
         #################
-        self.utilities.setText("")
-        self.rentalOfProperty.setText("")
-        self.loanRepayment.setText("")
-        self.education.setText("")
-        self.healthCare.setText("")
+        self.utilities.setText("") # not
+        self.rentalOfProperty.setText("") # not
+        self.loanRepayment.setText("") # not
+        self.education.setText("") # not
+        self.healthCare.setText("") # nor
+
+        #####################
         self.nutrion.setText("")
         self.clothing.setText("")
         self.housewares.setText("")
@@ -34,8 +38,32 @@ class ExampleApp(QtWidgets.QMainWindow, ui.Ui_MainWindow):
         self.fines.setText("")
         self.otherExpenses.setText("")
 
-    def Check(self):
-        print(self.inpWage.text())
+    def Run(self):
+        maxNutrionIncome = 10000 # питание
+        maxDirectionsIncome =  4500 # проезд
+        maxHousewaresIncome = 500 # предметы домашнего обихода
+        maxClothingIncome = 1000 # одежда
+        maxFinesIncome = 0 # штрафы
+        maxFunIncome = 1000 # развлечения
+        maxAnotherIncome = 1000 # остальные расходы
+
+        try:
+            realNutrionIncome = int(self.nutrion.text())
+            realDirectionsIncome = int(self.directions.text())
+            realHouseWaresIncome = int(self.housewares.text())
+            realClothingIncome = int(self.clothing.text())
+            realFinesIncome = int(self.fines.text())
+            realFunIncome = int(self.fun.text())
+            realAnotherIncome = int(self.anotherIncome.text())
+        except ValueError:
+            QMessageBox.about(self, "Ошибка","В некоторых полях введены не числа!")
+        
+
+
+
+
+
+
 
 def main():
         app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
