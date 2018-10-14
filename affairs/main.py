fafc8f9d37165fc67f227c4d1a21d28aa11fa045
prazd <alexokda@list.ru>
2018-10-15 02:57:51 +0300

    add

--- affairs/main.py
+++ affairs/main.py
@@ -8,12 +8,31 @@ class ExampleApp(QtWidgets.QMainWindow, ui.Ui_Affairs):
         super().__init__()
         self.setupUi(self) 
         # self.clear.clicked.connect(self.Clear)
-        self.Run.clicked.connect(self.Check)
+        self.add.clicked.connect(self.AddAffair)
     
-    def Check(self):
-        self.fListWidget.addItem("...")
+    def AddAffair(self):
+        if len(self.affair.text()) == 0:
+            QMessageBox.about(self, "Ошибка","Вы не записали задачу")
+            return
+        
+        fYesState = self.fYes.isChecked()
+        fNoState = self.fNo.isChecked()
+        sYesState = self.sYes.isChecked()
+        sNoState = self.sNo.isChecked()
 
-
+        if fYesState == True and sYesState == True:
+            self.fListWidget.addItem(self.affair.text()) # 1
+        if fYesState == True and sNoState == True:
+            self.sListWidget.addItem(self.affair.text()) # 2
+        if fNoState == True and sYesState == True:
+            self.tListWidget.addItem(self.affair.text()) # 3
+        if fNoState == True and sNoState == True:
+            self.lListWidget.addItem(self.affair.text()) # 4
+        
+    def Run(self):
+        pass
+        
+        
 def main():
         app = QtWidgets.QApplication(sys.argv)  
         window = ExampleApp()  
